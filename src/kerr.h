a4aa8969905de9fdfa265810407e8eb39b1d4c9c
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2018-11-07 22:09:12 +0100

    Fixed a typo

--- src/kerr.h
+++ src/kerr.h
@@ -7,10 +7,10 @@ public:
     chi3 = 4.0/3.0 * Constants::epsilon_0 * Constants::c * n2 * pressure;
   }
 
-  void calculate_temporal_response(const Radial& electric_field, const Array2D<double>& electron_density, Radial& response) override {
+  void calculate_temporal_response(const Radial& electric_field, const Array2D<double>&, Radial& response) override {
     for (int i = 0; i < response.Nradius; ++i) {
       for (int j = 0; j < response.Ntime; ++j) {
-        const double E = field.rt(i, j).real();
+        const double E = electric_field.rt(i, j).real();
         response.rt(i, j) = Constants::epsilon_0 * chi3 * std::pow(E, 3);
       }
     }
