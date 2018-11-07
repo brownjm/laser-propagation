35919b0c7a4b8725d712fbf38cb39e44e3a97ac0
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2018-11-07 17:32:25 +0100

    Added ionization base class and class for reading tabulated rates from a file. New feature is that ionization fills in the electron density.

--- src/ionization.cc
+++ src/ionization.cc
@@ -1,15 +1,31 @@
 #include "ionization.h"
 #include "io.h"
+#include "radial.h"
+#include "constants.h"
 
 namespace Ionization {
 
-  Rate::Rate(const std::string& filename) {
+  Tabulated::Tabulated(const std::string& filename, double density_of_neutrals, double ionizing_fraction, double pressure, double scaling)
+    :filename(filename), density_of_neutrals(density_of_neutrals),
+     ionizing_fraction(ionizing_fraction), pressure(pressure), scaling(scaling) {
+    std::vector<double> intensities, rates;
     IO::read(filename, intensities, rates);
-    interp = std::make_unique<Util::Interpolate>(intensities, rates);
+    interp = std::make_unique<Interpolate>(intensities, rates);
   }
 
-  double Rate::operator()(double intensity) {
-    return interp->operator()(intensity);
+  void Tabulated::calculate_electron_density(const Radial& electric_field,
+                                             Array2D<double>& electron_density) {
+    const double dt = electric_field.time[1] - electric_field.time[0];
+    for (int i = 0; i < electric_field.Nradius; ++i) {
+      Util::IntegratorTrapz integrator(dt);
+      for (int j = 0; j < electric_field.Ntime; ++j) {
+        double E = electric_field.rt(i, j).real();
+        double I = 0.5 * Constants::epsilon_0 * Constants::c * std::pow(E, 2);
+        double rate = scaling * interp->operator()(2*I);
+        double probability = integrator.add(rate);
+        electron_density(i, j) = density_of_neutrals * pressure * ionizing_fraction * probability;
+      }
+    }
   }
   
 }
--- src/ionization.h
+++ src/ionization.h
@@ -4,17 +4,30 @@
 #include <vector>
 #include <memory>
 #include "interpolate.h"
+#include "array2d.h"
+#include "util.h"
+
+class Radial;
+
 
 namespace Ionization {
-
-  class Rate {
+  
+  class Ionization {
   public:
-    Rate(const std::string& filename);
-    double operator()(double intensity);
+    virtual void calculate_electron_density(const Radial& electric_field,
+                                            Array2D<double>& electron_density) = 0;
+  };
+
+  class Tabulated : public Ionization {
+  public:
+    Tabulated(const std::string& filename, double density_of_neutrals, double ionizing_fraction, double pressure, double scaling);
+    void calculate_electron_density(const Radial& electric_field,
+                                    Array2D<double>& electron_density) override;
 
   private:
-    std::vector<double> intensities, rates;
-    std::unique_ptr<Util::Interpolate> interp;
+    std::string filename;
+    double density_of_neutrals, ionizing_fraction, pressure, scaling;
+    std::unique_ptr<Interpolate> interp;
   };
 
 
