bfe8a8f44e0bc08e801ef6b3554cb77ae5e800d0
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2018-11-07 17:29:08 +0100

    reorganized interpolation class

--- src/interpolate.cc
+++ src/interpolate.cc
@@ -4,32 +4,25 @@
 #include <iomanip>
 #include <stdexcept>
 
-namespace Util {
-  Interpolate::Interpolate(std::vector<double> xx, std::vector<double> yy)
-    :xpts(xx), ypts(yy) {
-    xmin = xpts.front();
-    xmax = xpts.back();
-    spline = gsl_spline_alloc(gsl_interp_linear, xpts.size());
-    gsl_spline_init(spline, xpts.data(), ypts.data(), xpts.size());
-    acc = gsl_interp_accel_alloc();
-  }
-
-  Interpolate::~Interpolate() {
-    gsl_spline_free(spline);
-    gsl_interp_accel_free(acc);
-  }
-
-  double Interpolate::operator()(double x) {
-    //return gsl_spline_eval(spline, x, acc);
-    if ((x >= xmin) && (x <= xmax)) {
-      return gsl_spline_eval(spline, x, acc);
-    }
-    else {
-      std::ostringstream oss;
-      oss << "Interpolation Error: ";
-      oss << std::scientific << x << " is not within range (";
-      oss << std::scientific << xmin << ", " << std::scientific << xmax << ")\n";
-      throw std::runtime_error(oss.str());
-    }
-  }
+std::runtime_error generate_interpolation_error(double x, double xmin, double xmax) {
+  std::ostringstream oss;
+  oss << "Interpolation Error: ";
+  oss << std::scientific << x << " is not within range (";
+  oss << std::scientific << xmin << ", " << std::scientific << xmax << ")\n";
+  return std::runtime_error(oss.str());
 }
+
+Interpolate::Interpolate(std::vector<double> xx, std::vector<double> yy)
+  :xpts(xx), ypts(yy) {
+  xmin = xpts.front();
+  xmax = xpts.back();
+  spline = gsl_spline_alloc(gsl_interp_linear, xpts.size());
+  gsl_spline_init(spline, xpts.data(), ypts.data(), xpts.size());
+  acc = gsl_interp_accel_alloc();
+}
+
+Interpolate::~Interpolate() {
+  gsl_spline_free(spline);
+  gsl_interp_accel_free(acc);
+}
+
--- src/interpolate.h
+++ src/interpolate.h
@@ -2,24 +2,29 @@
 #define INTERPOLATE_H_
 
 #include <vector>
+#include <iomanip>
 #include <gsl/gsl_spline.h>
 
-namespace Util {
+std::runtime_error generate_interpolation_error(double x, double xmin, double xmax);
   
-  class Interpolate {
-  public:
-    Interpolate(std::vector<double> xpts, std::vector<double> ypts);
-    ~Interpolate();
+class Interpolate {
+public:
+  Interpolate(std::vector<double> xpts, std::vector<double> ypts);
+  ~Interpolate();
+  
+  
+  double operator()(double x) {
+    if ((x >= xmin) && (x <= xmax)) return gsl_spline_eval(spline, x, acc);
+    else throw generate_interpolation_error(x, xmin, xmax);
+  }
+  
+private:
+  std::vector<double> xpts, ypts;
+  double xmin, xmax;
+  gsl_spline* spline;
+  gsl_interp_accel* acc;
+};
 
-    double operator()(double x);
 
-  private:
-    std::vector<double> xpts, ypts;
-    double xmin, xmax;
-    gsl_spline* spline;
-    gsl_interp_accel* acc;
-  };
-
-}
 
 #endif // INTERPOLATE_H_
