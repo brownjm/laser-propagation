f33d07a12802f55fd9f88f6dff4a33b153d120d6
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2018-11-07 22:09:37 +0100

    added nonlinear absorption source term

--- src/nonlinear_absorption.h
+++ src/nonlinear_absorption.h
@@ -2,5 +2,21 @@
 
 class NonlinearAbsorption : public NonlinearResponse {
 public:
-  NonlinearAbsorption();
+  NonlinearAbsorption(double Ui, double density_of_neutrals, double pressure, double fraction, double scaling, std::unique_ptr<Ionization::Ionization> rate)
+    :Ui(Ui), density_of_neutrals(density_of_neutrals), pressure(pressure),
+     fraction(fraction), scaling(scaling), rate(std::move(rate)) {}
+
+  void calculate_temporal_response(const Radial& electric_field, const Array2D<double>& electron_density, Radial& response) override {
+    for (int i = 0; i < response.Nradius; ++i) {
+      for (int j = 0; j < response.Ntime; ++j) {
+        const double E = electric_field.rt(i, j).real();
+        const double I = 0.5 * Constants::epsilon_0 * Constants::c * std::pow(E, 2);
+        const double W = scaling * rate->ionization_rate(2*I);
+        response.rt(i, j) = W / (I+1) * Ui * (fraction*density_of_neutrals*pressure - electron_density(i, j)) * Constants::epsilon_0 * Constants::c * E;
+      }
+    }
+  }
+  
+  double Ui, density_of_neutrals, pressure, fraction, scaling;
+  std::unique_ptr<Ionization::Ionization> rate;
 };
