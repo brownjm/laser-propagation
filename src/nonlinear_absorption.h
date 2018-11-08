96f2c107f7bd948f5838024ee4d9444d00611b7b
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2018-11-08 15:14:09 +0100

    Update NonlinearAbsorption to use new Ionization::Rate class

--- src/nonlinear_absorption.h
+++ src/nonlinear_absorption.h
@@ -2,21 +2,25 @@
 
 class NonlinearAbsorption : public NonlinearResponse {
 public:
-  NonlinearAbsorption(double Ui, double density_of_neutrals, double pressure, double fraction, double scaling, std::unique_ptr<Ionization::Ionization> rate)
-    :Ui(Ui), density_of_neutrals(density_of_neutrals), pressure(pressure),
-     fraction(fraction), scaling(scaling), rate(std::move(rate)) {}
+  NonlinearAbsorption(double ionization_potential, double density_of_neutrals,
+                      double pressure, double fraction, Ionization::Rate& ioniz)
+    :ionization_potential(ionization_potential),
+     density_of_neutrals(density_of_neutrals*pressure),
+     fraction(fraction), rate(ioniz) {}
 
-  void calculate_temporal_response(const Radial& electric_field, const Array2D<double>& electron_density, Radial& response) override {
+  void calculate_temporal_response(const Radial& electric_field,
+                                   const Array2D<double>& electron_density,
+                                   Radial& response) override {
     for (int i = 0; i < response.Nradius; ++i) {
       for (int j = 0; j < response.Ntime; ++j) {
         const double E = electric_field.rt(i, j).real();
+        const double W = rate.ionization_rate(E);
         const double I = 0.5 * Constants::epsilon_0 * Constants::c * std::pow(E, 2);
-        const double W = scaling * rate->ionization_rate(2*I);
-        response.rt(i, j) = W / (I+1) * Ui * (fraction*density_of_neutrals*pressure - electron_density(i, j)) * Constants::epsilon_0 * Constants::c * E;
+        response.rt(i, j) = W / (I+1) * ionization_potential * (fraction*density_of_neutrals - electron_density(i, j)) * Constants::epsilon_0 * Constants::c * E;
       }
     }
   }
   
-  double Ui, density_of_neutrals, pressure, fraction, scaling;
-  std::unique_ptr<Ionization::Ionization> rate;
+  double ionization_potential, density_of_neutrals, fraction;
+  Ionization::Rate& rate;
 };
