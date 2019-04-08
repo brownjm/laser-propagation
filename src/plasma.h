a09f67b275906795911d56523d8c0c8b589fa33d
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2019-04-08 16:34:15 +0200

    Fixed bug of wrong initial value for J(t) from plasma

--- src/plasma.h
+++ src/plasma.h
@@ -18,7 +18,7 @@ public:
     double exp = std::exp(-dt / collision_time);
     double eta = dt * std::pow(Constants::e, 2) / (2 * Constants::m_e);
     for (std::size_t i = 0; i < radius.size(); ++i) {
-      response(i, 0) += exp * electron_density(i, 0) * electric_field(i, 0).real();
+      response(i, 0) += eta * electron_density(i, 0) * electric_field(i, 0).real();
       for (std::size_t j = 1; j < time.size(); ++j) {
         response(i, j) += exp * (response(i, j-1) + eta*electron_density(i, j-1)*electric_field(i, j-1).real()) + eta*electron_density(i, j)*electric_field(i, j).real();
       }
